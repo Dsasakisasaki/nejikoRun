26b37ece5afc2fd637a72abc0db8d4600a8b122b
sasaki <kunnrenn0909@gmail.com>
2022-02-07 14:20:40 +0900

    p260まで

---
+++ Assets/Script/NejikoController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NejikoController : MonoBehaviour
+{
+    CharacterController controller;
+    Animator animator;
+
+    Vector3 moveDirection = Vector3.zero;
+
+    public float gravity;
+    public float speedZ;
+    public float speedJump;
+
+    private void Start()
+    {
+        //必要なコンポーネントを自動取得
+        controller = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        //接地判定
+        if (controller.isGrounded)
+        {
+            //前進ベロシティの設定
+            if (Input.GetAxis("Vertical") > 0.0f)
+            {
+                moveDirection.z = Input.GetAxis("Vertical") * speedZ;
+            }
+            else
+            {
+                moveDirection.z = 0;
+            }
+
+            //方向の回転
+            transform.Rotate(0, Input.GetAxis("Horizontal") * 3, 0);
+
+            //ジャンプ処理
+            if (Input.GetButton("Jump"))
+            {
+                moveDirection.y = speedJump;
+                animator.SetTrigger("jump");
+            }
+        }
+
+        //重力分の力を毎フレーム追加
+        moveDirection.y -= gravity * Time.deltaTime;
+
+        //移動実行
+        Vector3 globalDirection = transform.TransformDirection(moveDirection);
+        controller.Move(globalDirection * Time.deltaTime);
+
+        //移動後設置してたらY方向の速度はリセット
+        if (controller.isGrounded) moveDirection.y = 0;
+
+        //速度が０以上なら走っているフラグをtrueにする
+        //IdleとRunアニメーションの制御
+        animator.SetBool("run", moveDirection.z > 0.0f);
+    }
+}
---
+++ Assets/Script/SimpleFollow.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleFollow : MonoBehaviour
+{
+    Vector3 diff;
+
+    //追従ターゲットプロパティ
+    public GameObject target;
+    public float followSpeed;
+
+    private void Start()
+    {
+        //追従距離の計算
+        diff = target.transform.position - transform.position;
+    }
+
+    private void Update()
+    {
+        //線形補間関数によるスムージング
+        transform.position = Vector3.Lerp(
+            transform.position,
+            target.transform.position - diff,
+            Time.deltaTime * followSpeed
+            );
+    }
+}
