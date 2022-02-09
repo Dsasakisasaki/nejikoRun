0aab453568b4c50f2a5a15a2798624c98a8babac
sasaki <kunnrenn0909@gmail.com>
2022-02-09 13:52:39 +0900

    p311まで

---
+++ Assets/Script/GameController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    public NejikoController nejiko;
+    public Text scoreText;
+    public LifePanel lifePanel;
+
+    public void Update()
+    {
+        //スコア更新
+        int score = CalcScore();
+        scoreText.text = $"Score :{score}m";
+        //ライフパネルを更新
+        lifePanel.UpdateLife(nejiko.Life());
+    }
+
+    int CalcScore()
+    {
+        //ねじこの走行距離をスコアとする
+        return (int)nejiko.transform.position.z;
+    }
+}
